883f3a3f017fadd3d06e1d65776c319504a2f182
Eric Chiu <ericchiu@Eric-Chius-MacBook-Pro.local>
2017-04-23 14:25:44 -0400

    successfully loaded city data and displayed on screen, some bugs commented as TODO

--- src/main.js
+++ src/main.js
@@ -5,16 +5,55 @@ import Framework from './framework'
 
 //////////////////////////////////////////////////////////////////////
 
+//More info on Promises:
+//http://stackoverflow.com/questions/31375531/how-to-use-promises-with-papaparse
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+//https://developers.google.com/web/fundamentals/getting-started/primers/promises
+
+Papa.parsePromise = function(file) {
+  return new Promise(function(resolve, reject) {
+    Papa.parse(file, { download: true, complete: resolve, error: reject });
+  });
+};
+
+var maxIncome = 1.0;
+var zipToIncome = new Map();
+var promise1 = Papa.parsePromise("https://echiu1997.github.io/FNAR266-A06/data/zip_median-income.csv").then(
+    function(results) { 
+        //skip i = 0 because that is the column title
+        for (var i = 1; i < results.data.length; i++) {
+            //0 is zip, 1 is income
+            zipToIncome.set(results.data[i][0], results.data[i][1]);
+            if (parseInt(results.data[i][1]) > maxIncome) {
+                maxIncome = parseInt(results.data[i][1]);
+            }
+        }
+    });
+
+var zipToLocation = new Map();
+var promise2 = Papa.parsePromise("https://echiu1997.github.io/FNAR266-A06/data/zip_lat_lng.csv").then(
+    function(results) { 
+        //skip i = 0 because that is the column title
+        for (var i = 1; i < results.data.length; i++) {
+            //0 is zip, 1 is lat, 2 is lng
+            var location = [];
+            location.push(results.data[i][1]);
+            location.push(results.data[i][2]);
+            zipToLocation.set(results.data[i][0], location);
+        }
+    });
+
 var cityToBoundary = new Map();
-var promise1 = new Promise((resolve, reject) => { 
-    cityToBoundary.set("New York City", [40.472818, -74.306861, 40.928760, -73.675888]);
-    if (cityToBoundary.size == 1) {
+var promise3 = new Promise((resolve, reject) => { 
+    cityToBoundary.set("New York City", [40.472818, -74.306861, 40.935851, -73.636735]);
+    cityToBoundary.set("San Francisco", [37.699182, -122.531662, 37.844152, -122.333223]);
+    if (cityToBoundary.size == 2) {
         resolve();
     }
 }); 
 
 var cityToZip = new Map();
-var promise2 = new Promise((resolve, reject) => { 
+var promise4 = new Promise((resolve, reject) => { 
     cityToZip.set("New York City", [10001, 10002, 10003, 10004, 10005, 10006, 10007, 10009, 10010, 10011, 
                                     10012, 10013, 10014, 10016, 10017, 10018, 10019, 10020, 10021, 10022, 
                                     10023, 10024, 10025, 10026, 10027, 10028, 10029, 10030, 10031, 10032, 
@@ -37,49 +76,18 @@ var promise2 = new Promise((resolve, reject) => {
                                     11418, 11419, 11420, 11421, 11422, 11423, 11424, 11425, 11426, 11427, 
                                     11428, 11429, 11430, 11432, 11433, 11434, 11435, 11436, 11451, 11691, 
                                     11692, 11693, 11694, 11697]);
-    if (cityToZip.size == 1) {
+    cityToZip.set("San Francisco", [94102, 94103, 94104, 94105, 94107, 94108, 94109, 94110, 94111, 94112, 
+                                    94114, 94115, 94116, 94117, 94118, 94121, 94122, 94123, 94124, 94127, 
+                                    94129, 94130, 94131, 94132, 94133, 94134, 94158]);
+    if (cityToZip.size == 2) {
         resolve();
     }
 }); 
 
-//http://stackoverflow.com/questions/31375531/how-to-use-promises-with-papaparse
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
-Papa.parsePromise = function(file) {
-  return new Promise(function(resolve, reject) {
-    Papa.parse(file, { download: true, complete: resolve, error: reject });
-  });
-};
+//var normalizedData1 = [];
+//var normalizedData2 = [];
 
-var maxIncome = 1.0;
-var zipToIncome = new Map();
-var promise3 = Papa.parsePromise("https://echiu1997.github.io/FNAR266-A06/data/zip_median-income.csv").then(
-    function(results) { 
-        //skip i = 0 because that is the column title
-        for (var i = 1; i < results.data.length; i++) {
-            //0 is zip, 1 is income
-            zipToIncome.set(results.data[i][0], results.data[i][1]);
-            if (parseInt(results.data[i][1]) > maxIncome) {
-                maxIncome = parseInt(results.data[i][1]);
-                console.log("maxIncome ZIP: " + results.data[i][0] + " is: " + maxIncome);
-            }
-        }
-        console.log("final maxIncome: " + maxIncome);
-    });
-
-var zipToLocation = new Map();
-var promise4 = Papa.parsePromise("https://echiu1997.github.io/FNAR266-A06/data/zip_lat_lng.csv").then(
-    function(results) { 
-        //skip i = 0 because that is the column title
-        for (var i = 1; i < results.data.length; i++) {
-            //0 is zip, 1 is lat, 2 is lng
-            var location = [];
-            location.push(results.data[i][1]);
-            location.push(results.data[i][2]);
-            zipToLocation.set(results.data[i][0], location);
-        }
-    });
-
-var normalizedData = [];
+/*
 Promise.all([promise1, promise2, promise3, promise4]).then(values => { 
     var latitudeStep = (planeRadius*2) / (cityToBoundary.get("New York City")[2] - cityToBoundary.get("New York City")[0]);
     var longitudeStep = (planeRadius*2) / (cityToBoundary.get("New York City")[3] - cityToBoundary.get("New York City")[1]);
@@ -90,11 +98,12 @@ Promise.all([promise1, promise2, promise3, promise4]).then(values => {
         var zipIncome = parseInt(zipToIncome.get(zip))/maxIncome;
         //double check if any data is missing in one of the files
         if (!isNaN(zipLat) && !isNaN(zipLng) && !isNaN(zipIncome)) {
-            //console.log(zip + ": " + zipLat + ", " + zipLng + ", " + zipIncome);
+            console.log(zip + ": " + zipLat + ", " + zipLng + ", " + zipIncome);
             normalizedData.push(new THREE.Vector3(zipLat, zipLng, zipIncome));
         }
     }
 });
+*/
 
 /*
 var zipToIncome = new Map();
@@ -129,6 +138,7 @@ Papa.parse("https://echiu1997.github.io/FNAR266-A06/data/zip_lat_lng.csv", {
 ////////////////////////////////////////////////////////////////////////
 
 var Sliders = function() {
+    this.variance = 1.0;
     this.lineThickness = 0.3;
     this.lineFrequency = 0.6;
 };
@@ -180,20 +190,8 @@ var noiseMaterial2 = new THREE.ShaderMaterial( {
     side: THREE.DoubleSide
 } );
 
-/*
-var latitudeStep = (planeRadius*2) / (cityToBoundary.get("New York City")[2] - cityToBoundary.get("New York City")[0]);
-var longitudeStep = (planeRadius*2) / (cityToBoundary.get("New York City")[3] - cityToBoundary.get("New York City")[1]);
-for (var i = 0; i < cityToZip.get("New York City").length; i++) {
-    var zip = cityToZip.get("New York City")[i];
-    var zipLat = (zipToLocation.get(zip)[0] - cityToBoundary.get("New York City")[0]) * latitudeStep - planeRadius;
-    var zipLng = (zipToLocation.get(zip)[1] - cityToBoundary.get("New York City")[1]) * longitudeStep - planeRadius;
-    console.log(zipLat + ", " + zipLng);
-    normalizedData.push(new THREE.Vector3(zipLat, zipLng, zipToIncome.get(zip)));
-}
-*/
-
 // create the shader material      
-var dataMaterial1 = new THREE.ShaderMaterial( {
+var dataMaterial = new THREE.ShaderMaterial( {
     uniforms: {
         // float initialized to 0
         time: { type: "f", value: 0.0 },
@@ -203,13 +201,11 @@ var dataMaterial1 = new THREE.ShaderMaterial( {
         amp: { type: "f", value: 10.0 },
 
         lineThickness: { type: "f", value: 0.3},
-        lineFrequency: { type: "f", value: 0.6},
-
-        data: { type: "v3v", value: normalizedData}
+        lineFrequency: { type: "f", value: 0.6}
     },
     vertexShader: require('./shaders/data-vert.glsl'),
-    fragmentShader: require('./shaders/data-frag.glsl'),
-    side: THREE.DoubleSide
+    fragmentShader: require('./shaders/data-frag.glsl')
+    //TODO: FIX TRANSPARENECY
 } );
 
 ////////////////////////////////////////////////////////////////////
@@ -243,32 +239,78 @@ function onLoad(framework) {
     camera.position.set(0, 50, 100);
     camera.lookAt(new THREE.Vector3(0,0,0));
 
-    geometry1 = new THREE.PlaneGeometry(2*planeRadius, 2*planeRadius, 50, 50);
-    plane1 = new THREE.Mesh(geometry1, dataMaterial1);
-    plane1.position.x = 60;
-    rotateAroundObjectAxis(plane1, new THREE.Vector3(1,0,0), -Math.PI/2.0);
-    scene.add(plane1);
+    // setup plane for cities
+    computeData("New York City", geometry1, plane1, scene, new THREE.Vector3(60, 0, 0));
+    computeData("San Francisco", geometry2, plane2, scene, new THREE.Vector3(-60, 0, 0));
 
-    geometry2 = new THREE.PlaneGeometry(2*planeRadius, 2*planeRadius, 50, 50);
-    plane2 = new THREE.Mesh(geometry2, noiseMaterial2);
-    plane2.position.x = -60;
-    rotateAroundObjectAxis(plane2, new THREE.Vector3(1,0,0), -Math.PI/2.0);
-    scene.add(plane2);
-
-    gui.add(sliders, 'lineThickness', 0.1, 0.5).onChange(function(newVal) {
-        dataMaterial1.uniforms[ 'lineThickness' ].value = newVal;
-        noiseMaterial2.uniforms[ 'lineThickness' ].value = newVal;
+    gui.add(sliders, 'variance', 1.0, 3.0).step(1.0).onChange(function(newVal) {
+        sliders.variance = newVal;
     });
-    gui.add(sliders, 'lineFrequency', 0.1, 0.8).onChange(function(newVal) {
-        dataMaterial1.uniforms[ 'lineFrequency' ].value = newVal;
-        noiseMaterial2.uniforms[ 'lineFrequency' ].value = newVal;
+    gui.add(sliders, 'lineThickness', 0.1, 0.5).onChange(function(newVal) {
+        dataMaterial.uniforms[ 'lineThickness' ].value = newVal;
+    });
+    gui.add(sliders, 'lineFrequency', 0.1, 2.0).onChange(function(newVal) {
+        dataMaterial.uniforms[ 'lineFrequency' ].value = newVal;
     });
 }
 
-// Rotate an object around an arbitrary axis in object space
-var rotObjectMatrix;
+function computeData(city, geometry, plane, scene, position) {
+    
+    var normalizedData = [];
+    geometry = new THREE.PlaneGeometry(2*planeRadius, 2*planeRadius, 50, 50);
+
+    //perform all these operations after all the data is loaded
+    Promise.all([promise1, promise2, promise3, promise4]).then(values => { 
+        var latitudeStep = (planeRadius*2) / (cityToBoundary.get(city)[2] - cityToBoundary.get(city)[0]);
+        var longitudeStep = (planeRadius*2) / (cityToBoundary.get(city)[3] - cityToBoundary.get(city)[1]);
+        for (var i = 0; i < cityToZip.get(city).length; i++) {
+            var zip = cityToZip.get(city)[i].toString();
+            var zipLng = (zipToLocation.get(zip)[1] - cityToBoundary.get(city)[1]) * longitudeStep - planeRadius;
+            var zipLat = (zipToLocation.get(zip)[0] - cityToBoundary.get(city)[0]) * latitudeStep - planeRadius;
+            var zipIncome = parseInt(zipToIncome.get(zip))/maxIncome;
+            //double check if any data is missing in one of the files
+            if (!isNaN(zipLng) && !isNaN(zipLat) && !isNaN(zipIncome)) {
+                //NOTE: Longitude is X axis, Latitude is Y axis
+                normalizedData.push(new THREE.Vector3(zipLng, zipLat, zipIncome));
+            }
+        }
+
+        //vertex manipulation using gaussian distribution
+        for (var i = 0; i < geometry.vertices.length; i++) {
+            var accumulatedHeight = 0.0;
+
+            for (var j = 0; j < normalizedData.length; j++) {
+                var x2 = (geometry.vertices[i].x - normalizedData[j].x) * (geometry.vertices[i].x - normalizedData[j].x);
+                var y2 = (geometry.vertices[i].y - normalizedData[j].y) * (geometry.vertices[i].y - normalizedData[j].y);
+                var AePOW = normalizedData[j].z * Math.pow(2.71828, -(x2 + y2)/(2.0*sliders.variance*sliders.variance));
+                accumulatedHeight += AePOW;
+            }
+            
+            geometry.vertices[i].z = 10*accumulatedHeight;
+        }
+
+        plane = new THREE.Mesh(geometry, dataMaterial);
+        plane.position.x = position.x;
+        plane.position.y = position.y;
+        plane.position.z = position.z;
+        rotateAroundObjectAxis(plane, new THREE.Vector3(1,0,0), -Math.PI/2.0);
+        scene.add(plane);
+
+        //TODO: FIX ROTATION/REFERENCE
+        //TODO: FIX VARIANCE ADJUSTMENT
+        if (city == "New York City") {
+            plane1 = plane;
+        }
+        else {
+            plane2 = plane;
+        }
+        
+    });
+}
+
+// Rotate an object around an axis in object space
 function rotateAroundObjectAxis(object, axis, radians) {
-    rotObjectMatrix = new THREE.Matrix4();
+    var rotObjectMatrix = new THREE.Matrix4();
     rotObjectMatrix.makeRotationAxis(axis.normalize(), radians);
     object.matrix.multiply(rotObjectMatrix);
     object.rotation.setFromRotationMatrix(object.matrix);
